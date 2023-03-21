00c8871a0babb21b0b7dfe38361c2ccd2a9a8126
John <irelandjj@gmail.com>
2023-03-21 17:05:02 +0100

    Added authentication flow

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Pressable, Text, View } from "react-native";
 import styles from "./styles";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from "../../navigation/RootNavigator";
+import { AuthorizationService } from "../../services/AuthorizationService";
 
 type HomeScreenNavigationProp = StackNavigationProp<
     RootStackParamList,
@@ -13,6 +14,16 @@ type HomeScreenNavigationProp = StackNavigationProp<
 const HomeScreen = () => {
 
     const navigation = useNavigation<HomeScreenNavigationProp>();
+
+    const signOutUser = async () => {
+        try {
+            await AuthorizationService.signOut();
+            console.log('User signed out successfully');
+            navigation.navigate("SignIn");
+        } catch (error) {
+            console.error('Error signing out: ', error);
+        }
+    }
 
     const goToDiscoverRidersScreen = () => {
         navigation.navigate("Discover");
@@ -24,13 +35,18 @@ const HomeScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Pressable style={styles.button} onPress={() => goToDiscoverRidersScreen()}>
-                <Text style={styles.textButton}>Driver</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => goToPublishRiderTripScreen()}>
-                <Text style={styles.textButton}>Rider</Text>
-            </Pressable>
-        </View>);
+            <View style={styles.buttonsContainer}>
+                <Pressable style={styles.button} onPress={() => goToDiscoverRidersScreen()}>
+                    <Text style={styles.textButton}>Driver</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => goToPublishRiderTripScreen()}>
+                    <Text style={styles.textButton}>Rider</Text>
+                </Pressable>
+            </View>
+            <Button title="Sign Out" onPress={signOutUser} />
+        </View>
+
+    );
 };
 
 export default HomeScreen;
