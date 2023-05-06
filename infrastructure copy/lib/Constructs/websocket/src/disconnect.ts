a3e96422b0d6c600b4f0ca1443a78ae3338b53eb
Edgar <edgarfo1996@gmail.com>
2023-05-07 00:35:38 +0200

    test

--- infrastructure copy/lib/Constructs/websocket/src/disconnect.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-
-// eslint-disable-next-line func-style
-export const handler = async(
-  event: APIGatewayProxyEvent,
-  _context: Context 
-) => {
-  console.log(event, _context)
-}
