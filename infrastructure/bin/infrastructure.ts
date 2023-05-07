82d25b5b0239bd28211be63397fe9eddaba44771
Edgar <edgarfo1996@gmail.com>
2023-05-07 19:37:39 +0200

    cognito id

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { BackendStack } from '../lib/Stacks/backendStack';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = new cdk.App();
-new BackendStack(app, 'backendStack', { env: {account: cdk.Aws.ACCOUNT_ID, region: cdk.Aws.REGION }, description: 'This is the HopOn backend stack' });
+new BackendStack(app, 'backendStack',  {env: {
+  region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_ACCOUNT_ID,
+},
+description: 'This is the HopOn backend stack' });
